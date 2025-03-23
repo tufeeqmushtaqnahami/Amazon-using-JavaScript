306e14e8cc36a6b4ad2ae1b90d2c19a9952f762f
tufeeqmushtaqnahami <tufeeqmushtaq000@gmail.com>
2025-03-23 16:05:29 +0530

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,29 @@
-export let cart =[{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-},{
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+
+
+if(!cart){
+    cart =
+    [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+    },{
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
+
+
 
 export function addToCart(productId) {
 
     let matchingItem;
 
-    cart.forEach((item) => {
+    cart.forEach((cartItem) => {
         if (productId === cartItem.productId) {
             matchingItem = cartItem;
         }
@@ -23,7 +36,7 @@ export function addToCart(productId) {
             quantity: 1
         });
     }
-
+saveToStorage();
 }
 
 
@@ -37,5 +50,6 @@ export function removeFromCart(productId){
     });
   
     cart = newCart;
+    saveToStorage();
   };
   
