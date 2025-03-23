5bd57fcf8b9d48719d8e3240ec56e6162355970e
tufeeqmushtaqnahami <tufeeqmushtaq000@gmail.com>
2025-03-23 15:19:34 +0530

    delete products when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -39,7 +39,7 @@ cartSummaryHMTL +=`
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}" >
                     Delete
                   </span>
                 </div>
@@ -97,3 +97,16 @@ cartSummaryHMTL +=`
 
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHMTL;
+
+
+document.querySelectorAll('.js-delete-link').
+forEach((link)=>{
+link.addEventListener('click',()=>{
+const productId = link.dataset.productId ;
+removeFromCart(productId);
+
+});
+});
+
+
+
