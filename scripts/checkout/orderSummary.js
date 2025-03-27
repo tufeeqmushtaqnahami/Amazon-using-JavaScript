d6d9bcd4108b32a73604a4017c4dc9303b69ed57
tufeeqmushtaqnahami <tufeeqmushtaq000@gmail.com>
2025-03-27 21:14:14 +0530

    Created automated test for orderSummary.js file

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -32,7 +32,7 @@ export function renderOrderSummary() {
 
 
         cartSummaryHMTL += `
-        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+        <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
                   <div class="delivery-date">
                     Delivery date: ${dateString}
                   </div>
@@ -48,7 +48,7 @@ export function renderOrderSummary() {
                       <div class="product-price">
                       $${formatCurrency((matchingProduct.priceCents))}
                       </div>
-                      <div class="product-quantity">
+                      <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                         <span>
                           Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                         </span>
--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -13,6 +13,8 @@ describe("Test Suite : addToCart", () => {
       ]);
     });
     loadFromStorage();
+
+    
     addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
